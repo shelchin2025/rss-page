ab9310654a7895e703273eddb6d8cad8fffd3b40
shelchin2023 <149349405+shelchin2023@users.noreply.github.com>
2024-08-21 17:21:10 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,20 @@ export const SITE_DESCRIPTION =
 
 // Edit RSS sources
 export const feeds = [
-  {
+{
     "title": "CoinDesk",
     "url": "https://www.coindesk.com/arc/outboundfeeds/rss/",
   },
+    {
+    "title": "云风的 BLOG",
+    "url": "https://blog.codingnow.com/atom.xml",
+  },
+  {
+    "title": "Daniel Lemire's blog",
+    "url": "https://lemire.me/blog/feed/",
+  },
+  {
+    "title": "阮一峰的网络日志",
+    "url": "https://www.ruanyifeng.com/blog/atom.xml",
+  }
 ];
